28c9c2e40c2871633d3429b5ccaf55b002a254bd
alantiren <122862761+alantiren@users.noreply.github.com>
2023-11-03 13:55:46 +0300

    CurrencyXchange

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
 const dropList = document.querySelectorAll("form select"),
   fromCurrency = document.querySelector(".from select"),
   toCurrency = document.querySelector(".to select"),
-  getButton = document.querySelector("form button");
+  getButton = document.querySelector("form button"),
+  amountInput = document.querySelector("form input"),
+  exchangeRateTxt = document.querySelector("form .exchange-rate"),
+  convertHistory = document.getElementById("conversionHistory"),
+  clearHistoryButton = document.getElementById("clearHistory");
 
+const history = JSON.parse(localStorage.getItem("conversionHistory")) || [];
+
+// Populate conversion history
+function populateConversionHistory() {
+  convertHistory.innerHTML = history
+    .map(
+      (entry) =>
+        `<li>${entry.amount} ${entry.fromCurrency} = ${entry.result} ${entry.toCurrency}</li>`
+    )
+    .join("");
+}
+
+// Update conversion history in local storage
+function updateConversionHistory() {
+  localStorage.setItem("conversionHistory", JSON.stringify(history));
+}
+
+// Clear conversion history
+clearHistoryButton.addEventListener("click", () => {
+  history.length = 0;
+  updateConversionHistory();
+  populateConversionHistory();
+});
+
+// Load currency options from country_list
 for (let i = 0; i < dropList.length; i++) {
   for (let currency_code in country_list) {
     let selected =
@@ -32,6 +61,7 @@ function loadFlag(element) {
 
 window.addEventListener("load", () => {
   getExchangeRate();
+  populateConversionHistory();
 });
 
 getButton.addEventListener("click", (e) => {
@@ -39,31 +69,19 @@ getButton.addEventListener("click", (e) => {
   getExchangeRate();
 });
 
-const exchangeIcon = document.querySelector("form .icon");
-exchangeIcon.addEventListener("click", () => {
-  let tempCode = fromCurrency.value;
-  fromCurrency.value = toCurrency.value;
-  toCurrency.value = tempCode;
-  loadFlag(fromCurrency);
-  loadFlag(toCurrency);
-  getExchangeRate();
-});
-
-const amountInput = document.querySelector("form input");
-amountInput.addEventListener("input", () => {
-  getExchangeRate();
-});
-
 function getExchangeRate() {
-  const amount = document.querySelector("form input");
-  const exchangeRateTxt = document.querySelector("form .exchange-rate");
-  let amountVal = amount.value;
+  const amountVal = amountInput.value;
+  const fromCurrencyVal = fromCurrency.value;
+  const toCurrencyVal = toCurrency.value;
+
   if (amountVal === "" || amountVal === "0") {
-    amount.value = "1";
-    amountVal = 1;
+    amountInput.value = "1";
+    amountInput.value = 1;
   }
+
   exchangeRateTxt.innerText = "Getting exchange rate...";
-  let url = `https://v6.exchangerate-api.com/v6/4be82ca1fc36181e947027fa/latest/${fromCurrency.value}`;
+  let url = `https://v6.exchangerate-api.com/v6/4be82ca1fc36181e947027fa/latest/${fromCurrencyVal}`;
+
   fetch(url)
     .then((response) => {
       if (!response.ok) {
@@ -72,9 +90,20 @@ function getExchangeRate() {
       return response.json();
     })
     .then((result) => {
-      let exchangeRate = result.conversion_rates[toCurrency.value];
+      let exchangeRate = result.conversion_rates[toCurrencyVal];
       let totalExRate = (amountVal * exchangeRate).toFixed(2);
-      exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+      exchangeRateTxt.innerText = `${amountVal} ${fromCurrencyVal} = ${totalExRate} ${toCurrencyVal}`;
+
+      // Save the conversion in history
+      const conversionEntry = {
+        amount: amountVal,
+        fromCurrency: fromCurrencyVal,
+        toCurrency: toCurrencyVal,
+        result: totalExRate,
+      };
+      history.push(conversionEntry);
+      updateConversionHistory();
+      populateConversionHistory();
     })
     .catch((error) => {
       exchangeRateTxt.innerText = "Something went wrong";
